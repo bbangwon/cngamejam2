42e6ab5ec61613dc9873f042bf971c7aeeacfa1b
bbangwon <bbangwon.chung@gmail.com>
2021-11-28 03:56:36 +0900

    타이틀 돌리기

--- Assets/BreadOne/Scripts/TitleManager.cs
+++ Assets/BreadOne/Scripts/TitleManager.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using DG.Tweening;
 using Cysharp.Threading.Tasks;
+using System.Threading;
+using System;
 
 namespace cngamejam{
 
@@ -24,12 +26,17 @@ namespace cngamejam{
         Image Enemy;
 
         [SerializeField]
-        Image title;
+        Image title_1;
+
+        [SerializeField]
+        Image title_2;
 
         [SerializeField]
         Text pleaseAnyKey;
 
         bool interactable = false;
+
+        CancellationTokenSource cts = new CancellationTokenSource();
 
         private void Start()
         {
@@ -49,7 +56,8 @@ namespace cngamejam{
             pleaseAnyKey.DOFade(0f, 0f);
             player.transform.DOMoveY(-1100f, 0f);
             Enemy.transform.DOMoveY(-500f, 0f);
-            title.transform.DOMoveX(-1600f, 0f);
+            title_1.transform.DOMoveX(-1600f, 0f);
+            title_2.transform.DOScaleY(0f, 0f);
             light.DOFade(0f, 0f);
 
             await blackImage.DOFade(0f, 2f).AsyncWaitForCompletion();
@@ -60,8 +68,10 @@ namespace cngamejam{
 
             await Enemy.transform.DOMoveY(540f - 248f, 1f).SetDelay(0.5f).SetEase(Ease.InQuad).AsyncWaitForCompletion();
 
-            await title.transform.DOMoveX(960f - 310f, 1f).AsyncWaitForCompletion();
+            await title_1.transform.DOMoveX(960f - 310f, 1f).AsyncWaitForCompletion();
 
+            SwitchTitle().AttachExternalCancellation(cts.Token).Forget();
+            
             light.DOFade(1f, 0.1f).SetDelay(0.5f);
 
 
@@ -69,6 +79,31 @@ namespace cngamejam{
 
 
             interactable = true;
+        }
+
+
+        async UniTask SwitchTitle()
+        {
+            while(true)
+            {
+                try
+                {
+                    await UniTask.Delay(5000);
+                    await title_1.transform.DOScaleY(0f, 0.25f).AsyncWaitForCompletion();
+                    await title_2.transform.DOScaleY(1f, 0.25f).AsyncWaitForCompletion();
+                    await UniTask.Delay(5000);
+                    await title_2.transform.DOScaleY(0f, 0.25f).AsyncWaitForCompletion();
+                    await title_1.transform.DOScaleY(1f, 0.25f).AsyncWaitForCompletion();
+                }catch(OperationCanceledException e)
+                {
+                    Debug.Log("Cancel");
+                    if (e.CancellationToken == cts.Token)
+                    {
+                        return;
+                    }
+                        
+                }
+            }
         }
 
 
@@ -87,6 +122,12 @@ namespace cngamejam{
             SoundManager.Instance.Play("04_atk_pc");
             await UniTask.Delay(500);
             SceneManager.LoadScene(1);
+        }
+
+        private void OnDestroy()
+        {
+            cts.Cancel();
+            DOTween.Clear();
         }
     }
 }
