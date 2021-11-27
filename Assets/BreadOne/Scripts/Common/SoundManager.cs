0a9f29fa50f80d0e6f0729d98db36c00f97f6222
bbangwon <bbangwon.chung@gmail.com>
2021-11-27 12:56:25 +0900

    SoundManager 추가

---
+++ Assets/BreadOne/Scripts/Common/SoundManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Linq;
+
+namespace cngamejam{
+    public class SoundManager : MonoSingleton<SoundManager>
+    {
+        [SerializeField]
+        AudioClip[] audioClips;
+
+        AudioSource bgmSource;
+
+        private void Awake()
+        {
+            if(instance != null && instance != this)
+            {
+                DestroyImmediate(gameObject);
+                return;
+            }
+
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public void Play(string audioClipName)
+        {
+            Vector3 playPosition = Camera.main.transform.position;
+
+            AudioClip clip = audioClips.FirstOrDefault(c => c.name == audioClipName);
+
+            if (clip != null)
+                AudioSource.PlayClipAtPoint(clip, playPosition);
+        }
+
+        public void PlayBGM(string bgmName)
+        {
+            AudioClip bgmClip = audioClips.FirstOrDefault(clip => clip.name == bgmName);
+
+            if(bgmClip != null)
+            {
+                GameObject bgmObject = new GameObject("BGM");
+                bgmSource = bgmObject.AddComponent<AudioSource>();
+
+                bgmSource.clip = bgmClip;
+                bgmSource.loop = true;
+                bgmSource.Play();
+            }
+        }
+    }
+}
